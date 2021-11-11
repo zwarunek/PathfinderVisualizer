68305d6c204ff13a7558736024a96b94c6230208
zwarunek <zacharywarunek8@gmail.com>
2021-11-10 21:11:02 -0500

    deployment

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,2 @@
-@REM xcopy /s /E dist deployment\dist\
-@REM cd deployment
-@REM eb deploy PathfindingVisualizer
+ng run PathfindingVisualizer:prerender
 ng deploy --no-build --cname=pathfindingvisualizer.net
